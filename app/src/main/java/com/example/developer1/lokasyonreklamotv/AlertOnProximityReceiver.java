e72f2824deacc77826a579d30c7bc7972b59c150
Berat Nakip <g141210084@sakarya.edu.tr>
2018-03-14 13:47:43 +0300

    updated

--- app/src/main/java/com/example/developer1/lokasyonreklamotv/AlertOnProximityReceiver.java
+++ app/src/main/java/com/example/developer1/lokasyonreklamotv/AlertOnProximityReceiver.java
@@ -6,12 +6,16 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
-import static android.content.Context.NOTIFICATION_SERVICE;
+import java.lang.invoke.MethodHandles;
+
 
 /**
  * Created by Developer1 on 22.02.2018.
@@ -20,25 +24,30 @@ import static android.content.Context.NOTIFICATION_SERVICE;
 public class AlertOnProximityReceiver extends BroadcastReceiver {
     CharSequence text = "You are in....:";
     int duration = Toast.LENGTH_SHORT;
-    Toast toast;
+    /*private final Handler mHandler;
+    public AlertOnProximityReceiver(Handler handler) {
+            this.mHandler=handler;
+    }*/
     @Override
     public void onReceive(final Context context, final Intent intent){
-        Boolean getting_closer = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING,false);
-        String locationName = intent.getExtras().getString("name");
+        String key = LocationManager.KEY_PROXIMITY_ENTERING;
+        Boolean getting_closer = intent.getBooleanExtra(key,false);
+        final String locationName = intent.getExtras().getString("name");
         if(getting_closer){
-
-                toast = Toast.makeText(context,text+locationName,duration);
-                toast.show();
+            Toast toast;
+            toast = Toast.makeText(context,"Entered area..:"+locationName,duration);
+            toast.show();
             sendNotification(context,"Entered the area..:"+locationName);
          }else{
+            Toast toast;
             toast = Toast.makeText(context,"Exited area..:"+locationName,duration);
             toast.show();
             sendNotification(context,"Exited area..:"+locationName);
 
         }
+
+
         }
-
-
     public void sendNotification(Context mcontext,String msg){
 
         NotificationCompat.Builder mBuilder =
@@ -53,8 +62,9 @@ public class AlertOnProximityReceiver extends BroadcastReceiver {
                 (NotificationManager) mcontext.getSystemService(Context.NOTIFICATION_SERVICE);
 
 
-        mNotificationManager.notify(001, mBuilder.build());
+        mNotificationManager.notify(1, mBuilder.build());
     }
+
 
     }
 
--- app/src/main/java/com/example/developer1/lokasyonreklamotv/MainActivity.java
+++ app/src/main/java/com/example/developer1/lokasyonreklamotv/MainActivity.java
@@ -9,6 +9,9 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.nfc.Tag;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.TextViewCompat;
@@ -18,54 +21,25 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements LocationListener{
     private LocationManager locationManager;
     Context ctx;
     private TextView lat;
     private TextView lon;
-    private static int requestCode=0;
-    public final String PROX_ALERT_INTENT="com.example.developer1.lokasyonreklamotv.LOCATION_CHANGED";
-    String[][] locations = new String[][]{
-            {"Teknokent","40.741039","30.338614"},
-            {"Kampüs Durak","40.74031","30.32901"},
-            {"Bilg Müh Fakülte","40.74189","30.32579"},
-            {"Fen Edebiyat","40.74507","30.32869"},
-            {"Yemekhane","40.74402","30.33151"},
-            {"Enstitü","40.74281","30.33377"},
-            {"Besyo","40.74321","30.33863"},
-            {"Havana","40.74527","30.34346"},
-            {"Çakıroğlu Camii","40.74591","30.34693"},
-            {"Hakmar","40.74982","30.35246"},
-            {"Tapu","40.75289","30.3528"},
-            {"Tapu 2","40.75669","30.35607"},
-            {"Köprü","40.7592","30.3568"},
-            {"Zorlu","40.76144","30.3571"},
-            {"A101 durağı","40.76162","30.35881"},
-            {"Mavi Durak","40.76204","30.36167"},
-            {"Karakol","40.76569","30.36093"},
-            {"Serdivan Belediye","40.77169","30.36322"},
-            {"Sapak Camii","40.77257","30.36645"},
-            {"İbrahimağa Camii","40.77301","30.36936"},
-            {"Sakmar","40.74982","30.35246"},
-            {"Winhouse","40.77262","30.37266"},
-            {"Fuar mobilya","40.77282","30.37508"},
-            {"Yapı Kredi","40.77318","30.37702"},
-            {"Çilek Mobilya","40.77337","30.37854"},
-            {"Hakkı Demir Camii","40.7738","30.38027"},
-            {"Enza Home","40.7744","30.38187"},
-            {"Bellona","40.77476","30.38294"},
-            {"Köfteci İsmail","40.77512","30.38418"},
-            {"Kentpark","40.77553","30.38701"},
-            {"İmamhatip","40.77604","30.38903"},
-            {"Çark","40.7766","30.39168"}
-
-    };
+    private static int requestCode=1;
+    public final String PROX_ALERT_INTENT="com.example.developer1.lokasyonreklamotv.action.LOCATION_CHANGED";
+    HashMap<String,Double[]> locationList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationList= new HashMap<>();
+        addLocations(locationList);
         lon = findViewById(R.id.textView4);
         lat = findViewById(R.id.textView2);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -76,12 +50,52 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
             Toast.makeText(MainActivity.this, "First enable LOCATION ACCESS in settings.", Toast.LENGTH_LONG).show();
             return;
         }
-        for(int i=0;i<locations.length;i++){
-            addProximityAlert(Double.parseDouble(locations[i][1]),Double.parseDouble(locations[i][2]),20,locations[i][0]);
+      //  initialiseReceiver();
+        for (Map.Entry<String,Double[]> entry : locationList.entrySet()) {
+            String key = entry.getKey();
+            Double[] value = entry.getValue();
+            addProximityAlert(value[0],value[1],200,key);
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
-        AlertOnProximityReceiver a = new AlertOnProximityReceiver();
     }
+
+    private void addLocations(HashMap<String, Double[]> locationList) {
+        locationList.put("Teknokent",new Double[]{40.741039,30.338614});
+        locationList.put("Test",new Double[]{40.741039,30.338614});
+        locationList.put("Kampüs Durak",new Double[]{40.741039,30.338614});
+       locationList.put("Bilg Müh Fakülte",new Double[]{40.74189,30.32579});
+       locationList.put("Fen Edebiyat",new Double[]{40.74507,30.32869});
+       locationList.put("Yemekhane",new Double[]{40.74402,30.33151});
+       locationList.put("Enstitü",new Double[]{40.74281,30.33377});
+       locationList.put("Besyo",new Double[]{40.74321,30.33863});
+       locationList.put("Havana",new Double[]{40.74527,30.34346});
+       locationList.put("Çakıroğlu Camii",new Double[]{40.74591,30.34693});
+       locationList.put("Hakmar",new Double[]{40.74982,30.35246});
+       locationList.put("Tapu",new Double[]{40.75289,30.3528});
+       locationList.put("Tapu 2",new Double[]{40.75669,30.35607});
+       locationList.put("Köprü",new Double[]{40.7592,30.3568});
+       locationList.put("Zorlu",new Double[]{40.76144,30.3571});
+       locationList.put("A101 durağı",new Double[]{40.76162,30.35881});
+       locationList.put("Mavi Durak",new Double[]{40.76204,30.36167});
+       locationList.put("Karakol",new Double[]{40.76569,30.36093});
+       locationList.put("Serdivan Belediye",new Double[]{40.77169,30.36322});
+       locationList.put("Sapak Camii",new Double[]{40.77257,30.36645});
+       locationList.put("İbrahimağa Camii",new Double[]{40.77301,30.36936});
+       locationList.put("Sakmar",new Double[]{40.74982,30.35246});
+       locationList.put("Winhouse",new Double[]{40.77262,30.37266});
+       locationList.put("Fuar mobilya",new Double[]{40.77282,30.37508});
+       locationList.put("Yapı Kredi",new Double[]{40.77318,30.37702});
+       locationList.put("Çilek Mobilya",new Double[]{40.77337,30.37854});
+       locationList.put("Hakkı Demir Camii",new Double[]{40.7738,30.38027});
+       locationList.put("Enza Home",new Double[]{40.7744,30.38187});
+       locationList.put("Bellona",new Double[]{40.77476,30.38294});
+       locationList.put("Köfteci İsmail",new Double[]{40.77512,30.38418});
+       locationList.put("Kentpark",new Double[]{40.77553,30.38701});
+       locationList.put("İmamhatip",new Double[]{40.77604,30.38903});
+       locationList.put("Çark",new Double[]{40.7766,30.39168});
+
+    }
+
     @Override
     public void onLocationChanged(Location location) {
         String longitude = String.valueOf(location.getLongitude());
@@ -110,19 +124,37 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         // TODO Auto-generated method stub
 
     }
-    private void addProximityAlert(double latitude, double longitude, float radius, String poiName) {
+//    private void addProximityAlert(double latitude, double longitude, int radius, String poiName) {
+//
+//        Intent intent = new Intent(PROX_ALERT_INTENT);
+//        intent.putExtra("name",poiName);
+//        intent.putExtra("id",requestCode);
+//        PendingIntent proximityIntent = PendingIntent.getBroadcast(this, requestCode , intent, PendingIntent.FLAG_CANCEL_CURRENT);
+//        locationManager.addProximityAlert(
+//                latitude,
+//                longitude,
+//                (float)radius,
+//                -1,
+//                proximityIntent
+//        );
+//        requestCode++;
+//    }
 
-        Intent intent = new Intent(PROX_ALERT_INTENT);
+    private void addProximityAlert(double latitude, double longitude, int radius, String poiName) {
+        String newIntentName = PROX_ALERT_INTENT+"."+requestCode;
+        Intent intent = new Intent(newIntentName);
         intent.putExtra("name",poiName);
         intent.putExtra("id",requestCode);
-        PendingIntent proximityIntent = PendingIntent.getBroadcast(MainActivity.this, requestCode , intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent proximityIntent = PendingIntent.getBroadcast(this, requestCode , intent, PendingIntent.FLAG_CANCEL_CURRENT);
         locationManager.addProximityAlert(
                 latitude,
                 longitude,
-                radius,
+                (float)radius,
                 -1,
                 proximityIntent
         );
+        IntentFilter filter = new IntentFilter(newIntentName);
+        registerReceiver(new AlertOnProximityReceiver(), filter);
         requestCode++;
-
-    }}
+    }
+}
